6264d01e644186bd8fe4f1aba2590d978a68b54a
KRVPerera <rukshan.viduranga@gmail.com>
2016-06-19 20:40:25 +0530

    RBT and Treap nodes

---
+++ Python/Treap/Treap.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    This is a simple implementation of Treap
+"""
+import os, sys
+
+lib_path = os.path.abspath(os.path.join('..', 'basic'))
+sys.path.append(lib_path)
+
+from TreapNode import TreapNode
+
+class Treap(object):
+    def __init__(self):
+        self.root = None
+        self.size = 0
+
+    def size(self):
+        return self.size
+
+    def __len__(self):
+        return self.size
+
+    def __iter__(self):
+        return self.root.__iter__()
